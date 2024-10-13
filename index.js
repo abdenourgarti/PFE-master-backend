afe92804618e3d1cc90420542828b2d47415ecb2
Abdenour Garti <garti.abdenour@gmail.com>
2024-10-13 21:34:19 +0100

    change index

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ mongoose
   })
   .then(() => {
     console.log("Connected to MongoDB!");
-    app.listen(port, "0.0.0.0", () => {
-      console.log(`Server listening on port ${port}`);
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
     });
   })
   .catch((err) => console.error("Error connecting to MongoDB:", err));
